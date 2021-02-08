1025f396b70a7dbfd8506f20fe62f5e9513629f5
Alejandro Serrano <trupill@gmail.com>
2021-02-08 21:17:57 +0100

    Fix the fix

--- lib/langs/haskell.ex
+++ lib/langs/haskell.ex
@@ -206,7 +206,7 @@ defmodule Patternr.Haskell do
   def show({:wildcard, v}), do: v
   def show({:variable, {v, nil}}), do: v
   def show({:variable, {v, p}}), do: v <> "@" <> show_parens(p)
-  def show({:constructor, {c, args}}), do: show_several_spaces(c <> "", "", args)
+  def show({:constructor, {c, args}}), do: show_several_spaces(c <> " ", "", args)
   def show({:tuple, lst}), do: show_several("(", ")", lst)
   def show({:list, lst}), do: show_several("[", "]", lst)
   def show({:char, c}), do: "'#{c}'"
